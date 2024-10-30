17dbd67445d979a15d48deb80be63b51afa50ebd
linder <achie_31@yahoo.com>
2024-10-30 07:50:34 +0300

    done

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => 
+      tutorial.split(' ')
+              .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+              .join(' ')
+  );
 }
